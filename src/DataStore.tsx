c11c212604588f3056a70bc88b0c40df2779b467
pradyun <pradyun.setti@gmail.com>
2022-12-13 11:31:46 +1300

    Add: ds method for requesting data

--- src/DataStore.tsx
+++ src/DataStore.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+// import React from "react";
 
 class DataStore {
     constructor() {}
+
+    // store for any data, acting like a cache
+    private store = new Map<string, any>()
+
+    public request(id: string): any[] {
+        // if the data exists in the store, just return it
+        if (id in this.store) {
+            return this.store.get(id)
+        } else {
+            // else, fetch data from api and keep in the store
+            const data = this.getDataFor(id)
+            this.store.set(id, data)
+            return data
+        }
+    }
 
     public getDataFor(endpoint: string): any[] {
         return [] // todo
     }
 
 }
-export const ds = new DataStore()
+
+// singleton to share data across all graphs
+export const store = new DataStore()
 
