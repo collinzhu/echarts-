8c792b805e2d6ed007fe099ed6471aa7e330e57f
pradyun <pradyun.setti@gmail.com>
2022-12-13 10:44:51 +1300

    Add: datastore class

---
+++ src/DataStore.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class DataStore {
+    constructor() {}
+
+    public getDataFor(endpoint: string): any[] {
+        return [] // todo
+    }
+
+}
+export const ds = new DataStore()
+
