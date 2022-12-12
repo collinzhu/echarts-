ca685374c5b6cdfab3a0a1b4656fc06d93ebf3ef
Jason Xavier <94942712+jxav22@users.noreply.github.com>
2022-12-12 14:23:58 +1300

    feat: the chart now scales with height

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import PieChart from "./Components/PieChart";
+import useWindowDimensions from "./useWindowDimensions";
 
 function App() {
+  const { height } = useWindowDimensions();
+
   return (
-    <PieChart title="test" description="test" displayMax={15} height={500} />
+    <PieChart title="test" description="test" displayMax={15} height={height} />
   );
 }
 export default App;
