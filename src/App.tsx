33c8bf3bc6d91fd341cd42799b210047081a2d4b
Jason Xavier <94942712+jxav22@users.noreply.github.com>
2022-12-12 14:00:34 +1300

    feat: stopped BarChart.tsx from being displayed

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,4 @@
-import React, { useEffect } from "react";
-import ReactEcharts from "echarts-for-react";
-import inboundJson from "../data/Set0/inbound-message-counts.json";
-import BarChart from "./Components/BarChart";
-
-// "totalthreat": 0,
-// "totaldenypolicy": 0,
-// "domain": "fbl.vadesecure.com",
-// "totalspam": 0,
-// "totalclean": 72,
-// "totalallowpolicy": 357
-
-//state for the data
-interface IState {
-  totalThreat: number | any;
-  totalDenyPolicy: number | any;
-  domain: string | any;
-  totalSpam: number | any;
-  totalClean: number | any;
-  totalAllowPolicy: number | any;
-}
-
 function App() {
-  return (
-    <div>
-      <BarChart imc={inboundJson.data}></BarChart>
-    </div>
-  );
+  return <div />;
 }
 export default App;
