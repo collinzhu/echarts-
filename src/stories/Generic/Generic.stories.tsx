a469936e8089f035b02b977454b7ac8be0efa10f
Jason <94942712+jxav22@users.noreply.github.com>
2022-12-13 14:40:43 +1300

    feat: added all relevant datasets into generic component stories

--- src/stories/Generic/Generic.stories.tsx
+++ src/stories/Generic/Generic.stories.tsx
@@ -1,5 +1,12 @@
 import { data as data0 } from "../../data/Set1/0.json";
 import { data as data1 } from "../../data/Set1/1.json";
+import { data as data6 } from "../../data/Set1/6.json";
+import { data as data7 } from "../../data/Set1/7.json";
+import { data as data8 } from "../../data/Set1/8.json";
+import { data as data9 } from "../../data/Set1/9.json";
+import { data as data10 } from "../../data/Set1/10.json";
+import { data as data11 } from "../../data/Set1/11.json";
+import { data as data12 } from "../../data/Set1/12.json";
 import { data as data13 } from "../../data/Set1/13.json";
 import Generic from "../../Components/Generic/Generic";
 import { Story, Meta } from "@storybook/react";
@@ -13,11 +20,32 @@ const Template: Story = ({ keyValue, data, ...args }) => (
   <Generic keyValue={keyValue} data={data} {...args} />
 );
 
-export const firstDataSet = Template.bind({});
-firstDataSet.args = { keyValue: "domain", data: data0 };
+export const demo1 = Template.bind({});
+demo1.args = { keyValue: "domain", data: data0 };
 
-export const secondDataSet = Template.bind({});
-secondDataSet.args = { keyValue: "domain", data: data1 };
+export const demo2 = Template.bind({});
+demo2.args = { keyValue: "domain", data: data1 };
 
-export const thirteenthDataSet = Template.bind({});
-thirteenthDataSet.args = { keyValue: "address", data: data13 };
+export const demo6 = Template.bind({});
+demo6.args = { keyValue: "address", data: data6 };
+
+export const demo7 = Template.bind({});
+demo7.args = { keyValue: "address", data: data7 };
+
+export const demo8 = Template.bind({});
+demo8.args = { keyValue: "address", data: data8 };
+
+export const demo9 = Template.bind({});
+demo9.args = { keyValue: "address", data: data9 };
+
+export const demo10 = Template.bind({});
+demo10.args = { keyValue: "address", data: data10 };
+
+export const demo11 = Template.bind({});
+demo11.args = { keyValue: "address", data: data11 };
+
+export const demo12 = Template.bind({});
+demo12.args = { keyValue: "address", data: data12 };
+
+export const demo13 = Template.bind({});
+demo13.args = { keyValue: "address", data: data13 };
