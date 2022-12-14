61cbecb5bda16f1e17e9d897880d0919112e5ec6
collinzhu <345607645@qq.com>
2022-12-14 14:38:33 +1300

    typeEvent Story book added

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import inboundJson from "./example/inbound-message-counts.json"
 import unusedPolicy from "./example/14.json"
 import BarChart from "./Components/BarChart";
 import PieChart from "./Components/PieChart";
-
+import TypeOfEvent from "./Components/TypeOfEvent";
+import inboundpo from "./data/Set1/2.json"
+import outboundpo from "./data/Set1/3.json"
 // "totalthreat": 0,
 // "totaldenypolicy": 0,
 // "domain": "fbl.vadesecure.com",
@@ -26,6 +28,7 @@ function App() {
     <div>
       <BarChart imc={inboundJson.data}></BarChart>
        <PieChart up = {unusedPolicy.data}></PieChart>
+       <TypeOfEvent inbound={inboundpo.data} outbound = {outboundpo.data}></TypeOfEvent>
     </div>
   );
 }
