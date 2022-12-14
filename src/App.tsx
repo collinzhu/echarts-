9c957cf21b4d9de94756d466c1e167326ff425bb
Jason <94942712+jxav22@users.noreply.github.com>
2022-12-14 15:25:52 +1300

    refactor: moved Collins files into a subfolder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import inboundJson from "./example/inbound-message-counts.json"
-import unusedPolicy from "./example/14.json"
-import BarChart from "./Components/BarChart";
-import PieChart from "./Components/PieChart";
-import TypeOfEvent from "./Components/TypeOfEvent";
-import inboundpo from "./data/Set1/2.json"
-import outboundpo from "./data/Set1/3.json"
+import inboundJson from "./example/inbound-message-counts.json";
+import unusedPolicy from "./example/14.json";
+import BarChart from "./Components/Collin/BarChart";
+import PieChart from "./Components/Collin/PieChart";
+import TypeOfEvent from "./Components/Collin/TypeOfEvent";
+import inboundpo from "./data/Set1/2.json";
+import outboundpo from "./data/Set1/3.json";
 // "totalthreat": 0,
 // "totaldenypolicy": 0,
 // "domain": "fbl.vadesecure.com",
@@ -27,8 +27,11 @@ function App() {
   return (
     <div>
       <BarChart imc={inboundJson.data}></BarChart>
-       <PieChart up = {unusedPolicy.data}></PieChart>
-       <TypeOfEvent inbound={inboundpo.data} outbound = {outboundpo.data}></TypeOfEvent>
+      <PieChart up={unusedPolicy.data}></PieChart>
+      <TypeOfEvent
+        inbound={inboundpo.data}
+        outbound={outboundpo.data}
+      ></TypeOfEvent>
     </div>
   );
 }
---
+++ src/stories/Collin/PieChart.stories.tsx
@@ -0,0 +1,17 @@
+import PieChart from "../../Components/Collin/PieChart";
+import { Story, ComponentMeta } from "@storybook/react";
+import data from "../../example/14.json";
+
+export default {
+  title: "Piechart",
+  component: PieChart,
+};
+
+const Template: Story<typeof PieChart> = (args) => (
+  <PieChart up={[]} {...args} />
+);
+
+export const sample1 = Template.bind({});
+sample1.args = {
+  up: data.data,
+};
