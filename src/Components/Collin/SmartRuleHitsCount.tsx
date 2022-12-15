494fb0d3509fd3f8fd65f703e27e10eaae8a31d2
collinzhu <345607645@qq.com>
2022-12-15 14:53:38 +1300

    smr with storybook done

--- src/Components/Collin/SmartRuleHitsCount.tsx
+++ src/Components/Collin/SmartRuleHitsCount.tsx
@@ -6,19 +6,47 @@ interface ISmartRuleHitsCount{
     srd: any[];
 }
 
-let data = [
-    ['product', '2015', '2016', '2017'],
-    ['Matcha Latte', 43.3, 85.8, 93.7],
-    ['Milk Tea', 83.1, 73.4, 55.1],
-    ['Cheese Cocoa', 86.4, 65.2, 82.5],
-    ['Walnut Brownie', 72.4, 53.9, 39.1]
-  ]
+// let data = [
+//     ['product', '2015'],
+//     ['Matcha Latte', 43.3],
+//     ['Milk Tea', 83.1],
+//     ['Cheese Cocoa', 86.4],
+//     ['Walnut Brownie', 72.4]
+//   ]
 function SmartRuleHitsCount (props:ISmartRuleHitsCount){
     let ok = props.srd
-    console.log("data", ok);
+   // console.log("data", ok);
+    const yes = ok.map(data =>({
+      'name': data.rule, 'value': data.executioncount
+    }))
+    //console.log("yes",yes);
     
-
-   
+    let haha = [
+      {name:"Inbound - Quarantine Unscannable Attachments",value:2969},
+      {name: "Inbound - Quarantine Executables", value:76},
+      {name:"Inbound - Quarantine Suspicious Attachments",value:12},
+      {name: "Bypass Offensive Content Filter for Scheduled Reports", value:26338},
+      {name:"Inbound - Explicit Address Block List",value:7712},
+      {name:"Inbound - Bypass Explicit Address Block List",value:619},
+      {name:"Gmail Spam",value:111},
+      {name:"Drop Emails",value:136},
+      {name:"Inbound - Strip oledata files",value:60},
+      {name:"Whaling Protection",value:2},
+      {name:"icloud Spam",value:7}
+    
+    ]
+    const head = [
+      "rule", "rules"
+    ]
+    const body = haha.map((data) =>{
+      return [
+        data.name,
+        data.value
+      ]
+    })
+    console.log(body);
+     
+    const data = [head, ...body];
     const option = {
         legend: {},
         tooltip: {},
@@ -26,13 +54,10 @@ function SmartRuleHitsCount (props:ISmartRuleHitsCount){
           source: data,
         },
         xAxis: {},
-        yAxis: { type: "category" },
+        yAxis: {  type: "category"},
         series: [
           { type: "bar" },
-          { type: "bar" },
-          { type: "bar" },
-          { type: "bar" },
-          { type: "bar" },
+      
         ],
     };
     
