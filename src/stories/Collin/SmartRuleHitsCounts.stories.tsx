6e4b64b52ea325e04b5c6ad5c35cae2e74eaea97
Jason <94942712+jxav22@users.noreply.github.com>
2022-12-16 00:24:09 +1300

    Updated general styling for components under \Collin

--- src/stories/Collin/SmartRuleHitsCounts.stories.tsx
+++ src/stories/Collin/SmartRuleHitsCounts.stories.tsx
@@ -2,18 +2,15 @@ import SmartRuleHitsCount from "../../Components/Collin/SmartRuleHitsCount";
 import { Story, ComponentMeta } from "@storybook/react";
 
 export default {
-    title: "smartRuleHitsCount",
-    component: SmartRuleHitsCount,
+  title: "Smart Rules by Hit Count",
+  component: SmartRuleHitsCount,
 };
 
-
-
-
 const Template: Story<typeof SmartRuleHitsCount> = (args) => (
-    <SmartRuleHitsCount srd={[]}{...args} />
-  );
-  
+  <SmartRuleHitsCount srd={[]} {...args} />
+);
+
 //   sample1.args = {
 //     up: data.data
 // }
-  export const sample1 = Template.bind({});
+export const sample1 = Template.bind({});
