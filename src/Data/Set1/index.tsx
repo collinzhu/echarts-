cc7aa39be1d877ede3bfcdc8a91e098788be9b1a
Jason <94942712+jxav22@users.noreply.github.com>
2022-12-19 13:32:55 +1300

    added an index for imports from the second data set

---
+++ src/Data/Set1/index.tsx
@@ -0,0 +1,39 @@
+import data0 from "./0.json";
+import data1 from "./1.json";
+import data2 from "./2.json";
+import data3 from "./3.json";
+import data4 from "./4.json";
+import data5 from "./5.json";
+import data6 from "./6.json";
+import data7 from "./7.json";
+import data8 from "./8.json";
+import data9 from "./9.json";
+import data10 from "./10.json";
+import data11 from "./11.json";
+import data12 from "./12.json";
+import data13 from "./13.json";
+import data14 from "./14.json";
+import data15 from "./15.json";
+import data16 from "./16.json";
+import data17 from "./17.json";
+
+export {
+  data0,
+  data1,
+  data2,
+  data3,
+  data4,
+  data5,
+  data6,
+  data7,
+  data8,
+  data9,
+  data10,
+  data11,
+  data12,
+  data13,
+  data14,
+  data15,
+  data16,
+  data17,
+};
